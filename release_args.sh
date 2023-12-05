d5dcd2c2f1c5d1e64793b418ba593f697fadedd5
Niklas <niklas.hinze@cloudogu.com>
2023-12-05 13:19:22 +0100

    #9 Update versions in release process.

---
+++ release_args.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# this function will be sourced from release.sh and be called from release_functions.sh
+update_versions_modify_files() {
+  newReleaseVersion="${1}"
+  valuesYAML=k8s/helm/values.yaml
+  componentPatchTplYAML=k8s/helm/component-patch-tpl.yaml
+
+  yq -i ".job.image.tag = \"${newReleaseVersion}\"" "${valuesYAML}"
+  yq -i ".values.images.job = \"cloudogu/k8s-host-change:${newReleaseVersion}\" " "${componentPatchTplYAML}"
+}
+
+update_versions_stage_modified_files() {
+  valuesYAML=k8s/helm/values.yaml
+  componentPatchTplYAML=k8s/helm/component-patch-tpl.yaml
+
+  git add "${valuesYAML} ${componentPatchTplYAML}"
+}
