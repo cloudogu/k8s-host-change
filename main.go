cda32193688b8867d0bbdf6da4805265f8d0ef3f
Nico Franzeck <nico.franzeck@cloudogu.com>
2024-06-26 14:56:43 +0200

    update main.go
    also delete test as they only use mocks for the deps

--- main.go
+++ main.go
@@ -6,9 +6,11 @@ import (
 
 	ctrl "sigs.k8s.io/controller-runtime"
 
+	"github.com/cloudogu/k8s-host-change/pkg/alias"
 	"github.com/cloudogu/k8s-host-change/pkg/hosts"
 	"github.com/cloudogu/k8s-host-change/pkg/initializer"
 	"github.com/cloudogu/k8s-host-change/pkg/logging"
+	"github.com/cloudogu/k8s-registry-lib/registry"
 )
 
 var logger = ctrl.Log.WithName("k8s-host-change")
@@ -35,12 +37,14 @@ func run() error {
 		return err
 	}
 
-	cesReg, err := init.CreateCesRegistry()
+	globalConfig, err := registry.NewGlobalConfigReader(context.Background(), clientSet.CoreV1().ConfigMaps(namespace))
 	if err != nil {
 		return err
 	}
 
-	updater := hosts.NewHostAliasUpdater(clientSet, cesReg)
+	hostGenerator := alias.NewHostAliasGenerator(globalConfig)
+
+	updater := hosts.NewHostAliasUpdater(clientSet, hostGenerator)
 	err = updater.UpdateHosts(context.Background(), namespace)
 	if err != nil {
 		return err
